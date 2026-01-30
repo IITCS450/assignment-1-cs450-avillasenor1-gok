75bef0ee600db99d04cb5129c310d714eb2d63bd
Chameleon Cloud User <cc@avillasenor1.novalocal>
2026-01-30 11:24:42 -0500

    Finished code

--- src/runwait.c
+++ src/runwait.c
@@ -2,14 +2,54 @@
 #include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdio.h>
 static void usage(const char *a){fprintf(stderr,"Usage: %s <cmd> [args]\n",a); exit(1);}
 static double d(struct timespec a, struct timespec b){
  return (b.tv_sec-a.tv_sec)+(b.tv_nsec-a.tv_nsec)/1e9;}
+
+
 int main(int c,char**v){
-/* TODO : ADD CODE HERE
+	
+  	if(c < 2){
+        	usage(v[0]);
+    	}
 
-//test to check if I can commit
+    	struct timespec start;
+	struct timespec end;
 
-*/
+
+//start timer   
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	pid_t pid = fork();
+
+    	if(pid == 0){
+        	execvp(v[1], &v[1]);
+		return 1;
+   	 }
+   
+    	int status;
+
+    	if(waitpid(pid, &status, 0) == -1){
+        	perror("waitpid");
+        	return 1;
+	    }
+
+// end timer
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+    	double elapsed = d(start, end);
+
+    	printf("pid=%d ", pid);
+
+	printf("elapsed=%.3f ", elapsed);
+
+	if(WIFEXITED(status)){
+        	printf("exit=%d\n", WEXITSTATUS(status));
+		}
+	else if(WIFSIGNALED(status)){
+		printf("signal=%d\n", WTERMSIG(status));
+	}
+
  return 0;
 }
